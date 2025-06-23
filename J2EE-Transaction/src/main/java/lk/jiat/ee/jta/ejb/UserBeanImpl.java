01781d58f2ddb2d0ec0201efb473782db695159e
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-23 22:12:45 +0530

    Create UserBeanImpl.java

---
+++ J2EE-Transaction/src/main/java/lk/jiat/ee/jta/ejb/UserBeanImpl.java
@@ -0,0 +1,38 @@
+package lk.jiat.ee.jta.ejb;
+
+import jakarta.ejb.Stateless;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lk.jiat.ee.jta.ejb.local.UserBean;
+import lk.jiat.ee.jta.entity.User;
+import org.hibernate.Session;
+
+@Stateless
+public class UserBeanImpl implements UserBean {
+    @PersistenceContext(name = "JTAPA")
+    private EntityManager em;
+
+
+    @Override
+    public boolean login(String email, String password) {
+        return false;
+    }
+
+    @Override
+    public boolean register(String name, String email, String password, String contact) {
+
+       Session session = em.unwrap(Session.class);
+
+
+        User user = new User();
+        user.setName(name);
+        user.setEmail(email);
+        user.setPass(password);
+        user.setContact(contact);
+        
+        session.save(user);
+        em.persist(user);
+
+        return true;
+    }
+}
