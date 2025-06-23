7b2e2b66e01b4c31fc110eb4b7447c0a84032986
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-23 19:00:13 +0530

    Create User.java

---
+++ J2EE-Transaction/src/main/java/lk/jiat/ee/jta/entity/User.java
@@ -0,0 +1,57 @@
+package lk.jiat.ee.jta.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    private String name;
+    @Column(name = "password")
+    private String pass;
+    private String email;
+    private String contact;
+}
