7443553879ad05c01593ad456ff9ae8055c43bb7
ravi1280 <ravishkaindrajith9.9@gmail.com>
2025-06-23 23:43:43 +0530

    Update UserBeanImpl.java

--- J2EE-Transaction/src/main/java/lk/jiat/ee/jta/ejb/UserBeanImpl.java
+++ J2EE-Transaction/src/main/java/lk/jiat/ee/jta/ejb/UserBeanImpl.java
@@ -12,7 +12,6 @@ public class UserBeanImpl implements UserBean {
     @PersistenceContext(name = "JTAPA")
     private EntityManager em;
 
-
     @Override
     public boolean login(String email, String password) {
         return false;
@@ -20,18 +19,16 @@ public class UserBeanImpl implements UserBean {
 
     @Override
     public boolean register(String name, String email, String password, String contact) {
-
-       Session session = em.unwrap(Session.class);
-
-
+        Session  session =em.unwrap(Session.class);
         User user = new User();
         user.setName(name);
         user.setEmail(email);
         user.setPass(password);
         user.setContact(contact);
-        
+
         session.save(user);
-        em.persist(user);
+
+//        em.persist(user);
 
         return true;
     }
